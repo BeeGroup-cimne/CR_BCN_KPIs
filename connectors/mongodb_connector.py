bbeff9b7555050a9ee2eb2e1e3b7eeb452b8ad53
jose <manoloainsa@gmail.com>
2024-11-08 15:59:42 +0100

    ddbb connectors

---
+++ connectors/mongodb_connector.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from pymongo import MongoClient
+from config.config_loader import config
+
+
+def store_data_in_mongodb(collection_name, data):
+    # store kpi
+    client = MongoClient(config["mongo_db"]["uri"])
+    db = client[config["mongo_db"]["db"]]
+    collection = db[collection_name]
+    collection.insert_one(data)
+    # update calculation date
+    db[f"""{collection_name}_data"""].update_one({},
+                                                 {"$push": {"calculation_dates": datetime.now().date().isoformat()}},
+                                                 upsert=True)
+    client.close()
