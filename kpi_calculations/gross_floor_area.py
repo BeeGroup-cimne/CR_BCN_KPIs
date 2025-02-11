a8f1c2e443d03a3f83fd99587bcedb5d17fa6bc5
jose <manoloainsa@gmail.com>
2025-02-11 11:58:25 +0100

    gross_floor_area.py KPI

--- kpi_calculations/gross_floor_area.py
+++ kpi_calculations/gross_floor_area.py
@@ -2,27 +2,28 @@ from datetime import datetime
 from connectors.hbase_connector import fetch_data_from_hbase
 from connectors.neo4j_connector import fetch_data_from_neo4j
 from kpi_calculations.kpi_base import KPIBase
+import pandas as pd
 
 class GrossFloorArea(KPIBase):
     def __init__(self, kpi_name):
         super().__init__(mongo_collection_name=kpi_name)
 
     def extract_data(self):
-        query = f"""MATCH (n:s4bldg__Building)-[:geosp__hasArea]->(m:saref__Measurement)-[:saref__relatesToProperty]->(bigg__GrossFloorArea{{uri:"http://bigg-project.eu/ontology#GrossFloorArea"}}) RETURN apoc.map.fromPairs(collect([m.uri, m.saref__hasValue])) AS result"""
-        self.data["neo4j_data"] = fetch_data_from_neo4j(query)
+        # query = f"""MATCH (n:s4bldg__Building)-[:geosp__hasArea]->(m:saref__Measurement)-[:saref__relatesToProperty]->(bigg__GrossFloorArea{{uri:"http://bigg-project.eu/ontology#GrossFloorArea"}}) RETURN apoc.map.fromPairs(collect([m.uri, m.saref__hasValue])) AS result"""
+        # self.data["neo4j_data"] = fetch_data_from_neo4j(query)
+
+        file_path = "/Users/jose/Nextcloud/Beegroup/data/hypercadaster_ES/results/08900_br_results.pkl"
+        df = pd.read_pickle(file_path, compression="gzip")
+        self.data["neo4j_data"] = dict(zip(df['building_reference'], df["br__above_ground_built_area"] + df["br__below_ground_built_area"]))
 
     def calculate(self):
-        self.result = self.helper_transform_data(self.data["neo4j_data"][0]["result"])
+        self.result = self.helper_transform_data(self.data["neo4j_data"])
 
     def helper_transform_data(self, data):
-        mongo_data = {
-            "calculation_date": datetime.now().date().isoformat(),
-            "kpis": {}
-        }
-
-        for key, value in data.items():
-            start = key.find('Measurement-') + len('Measurement-')
-            end = key.find('-GrossFloorArea')
-            custom_id = key[start:end]
-            mongo_data["kpis"][custom_id] = int(value)
-        return mongo_data
+        return {
+                "calculation_date": datetime.now().date().isoformat(),
+                "kpis": {
+                    key: round(value, 2)
+                    for key, value in data.items()
+                }
+            }
