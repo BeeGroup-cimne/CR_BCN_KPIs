563829426bc8a73cddb371c196c805550300d8e7
jose <manoloainsa@gmail.com>
2025-02-27 12:22:05 +0100

    demographic unit(u/dwelling)

--- kpi_calculations/female_residents_per_building.py
+++ kpi_calculations/female_residents_per_building.py
@@ -7,6 +7,7 @@ from connectors.neo4j_connector import fetch_data_from_neo4j
 from kpi_calculations.kpi_base import KPIBase
 import pandas as pd
 from social_ES.utils_INE import INEPopulationAnualCensus
+from config.config_loader import config
 
 
 class FemaleResidentsPerBuilding(KPIBase):
@@ -17,7 +18,7 @@ class FemaleResidentsPerBuilding(KPIBase):
 
         # Social
         df = INEPopulationAnualCensus(
-            path="/Users/jose/Nextcloud/Beegroup/data/social_ES/data/INEPopulationAnualCensus",
+            path=f"{config['paths']['nextcloud']}/data/social_ES/data/INEPopulationAnualCensus",
             municipality_code="08019")['Sections']
 
         self.data["social"] = {
@@ -27,15 +28,21 @@ class FemaleResidentsPerBuilding(KPIBase):
         }
 
         # Residential area
-        file_path = "/Users/jose/Nextcloud/Beegroup/data/hypercadaster_ES/results/08900_br_results.pkl"
-        df = pd.read_pickle(file_path, compression="gzip")
-        self.data["area"] = dict(zip(df['building_reference'], df['br__area_without_communals']))
+        file_path = f"{config['paths']['nextcloud']}/data/hypercadaster_ES/results/08900_br_results.pkl"
+        hyper_df = pd.read_pickle(file_path, compression="gzip")
+        main_usage_list = ["Residential"]
+        self.data["area"] = dict(zip(hyper_df['building_reference'], hyper_df['br__area_without_communals']))
+        self.data["building_spaces"] = dict(zip(
+            hyper_df["building_reference"],
+            hyper_df["br__building_spaces"].map(
+                lambda d: sum(d.get(k, 0) for k in main_usage_list) if isinstance(d, dict) else 0)
+        ))
 
         # Relation
         query = f"""MATCH (b:s4bldg__Building)<-[:geosp__sfContains]-(ct:gn__parentADM5)
-                    WITH split(b.uri, "-")[1] AS referencia, substring(split(ct.uri, "-")[1], 0, 5) + substring(split(ct.uri, "-")[1], 6) AS clave
-                    WITH clave, collect(referencia) AS referencias
-                    RETURN apoc.map.fromPairs(collect([clave, referencias])) AS result"""
+                            WITH split(b.uri, "-")[1] AS referencia, substring(split(ct.uri, "-")[1], 0, 5) + substring(split(ct.uri, "-")[1], 6) AS clave
+                            WITH clave, collect(referencia) AS referencias
+                            RETURN apoc.map.fromPairs(collect([clave, referencias])) AS result"""
         self.data["neo4j_data"] = fetch_data_from_neo4j(query)
 
     def calculate(self):
@@ -57,14 +64,14 @@ class FemaleResidentsPerBuilding(KPIBase):
                 building_data = self.data['area'].get(building, {})
                 if isinstance(building_data, dict):
                     building_residential_area = building_data.get('Residential', 0)
-
-                if building_residential_area == 0:
+                building_spaces_data = self.data['building_spaces'].get(building, 0)
+                if building_spaces_data == 0:
                     continue
 
                 for year, social_value in self.data["social"].items():
                     self.data['result'][year][building] = (
                                 (building_residential_area / census_tract_residential_area) * social_value.get(
-                            census_tract, np.nan))
+                            census_tract, np.nan)) / building_spaces_data
 
         self.result = self.helper_transform_data(self.data["result"])
 
@@ -74,7 +81,6 @@ class FemaleResidentsPerBuilding(KPIBase):
                 "calculation_date": datetime(year=int(year), month=1, day=1).date().isoformat(),
                 "kpis": {
                     key: round(value, 2)
-                    # key: value
                     for key, value in values.items()
                 }
             }
