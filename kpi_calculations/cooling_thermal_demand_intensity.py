6770046fa56dc9fba5b327f262737880f908e06a
jose <manoloainsa@gmail.com>
2025-03-06 15:23:37 +0100

    energy indicators

---
+++ kpi_calculations/cooling_thermal_demand_intensity.py
@@ -0,0 +1,25 @@
+import json
+from datetime import datetime
+from connectors.hbase_connector import fetch_data_from_hbase
+from connectors.neo4j_connector import fetch_data_from_neo4j
+from kpi_calculations.kpi_base import KPIBase
+import geopandas as gpd
+import pandas as pd
+
+class CoolingThermalDemandIntensity(KPIBase):
+    def __init__(self, kpi_name):
+        super().__init__(mongo_collection_name=kpi_name)
+
+    def extract_data(self):
+        epc_df = pd.read_parquet("data/epc_predictor_results.parquet")
+        self.data["neo4j_data"] = dict(zip(epc_df['building_reference'], round(epc_df['Cooling__ThermalDemandIntensity'], 2)))
+
+    def calculate(self):
+        self.result = self.helper_transform_data(self.data["neo4j_data"])
+
+    def helper_transform_data(self, data):
+        return {
+            "calculation_date": datetime.now().date().isoformat(),
+            "kpis": data
+        }
+
