35c8c4e66161260ad63a27c8793b17ab3dd22411
jose <manoloainsa@gmail.com>
2025-06-27 13:41:13 +0200

    building code fix

--- kpi_calculations/building_code.py
+++ kpi_calculations/building_code.py
@@ -29,9 +29,17 @@ class BuildingCode(KPIBase):
 
     def calculate(self):
         INVALID_VALUES = {None, "--01", "150-", "1-01", "2-01"}
+        type_map = {
+            'SL': [1, 0, 0, 0, 0, 0],
+            'NTE': [0, 1, 0, 0, 0, 0],
+            'NBE': [0, 0, 1, 0, 0, 0],
+            'CTE2006': [0, 0, 0, 1, 0, 0],
+            'CTE2013': [0, 0, 0, 0, 1, 0],
+            'CTE2019': [0, 0, 0, 0, 0, 1],
+        }
         self.data["result"] = {}
         for k, v in self.data["neo4j_data"][0]['result'].items():
-            self.data["result"][k] = self.get_building_code(int(v) if v not in INVALID_VALUES else 0)
+            self.data["result"][k] = type_map[self.get_building_code(int(v) if v not in INVALID_VALUES else 0)]
         self.result = self.helper_transform_data(self.data["result"])
 
     def helper_transform_data(self, data):
